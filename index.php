7f9acdb8174e2e5efd4dc97365ef466abdac130b
Haro231 <1419172@rgu.ac.uk>
2016-05-03 19:37:30 +0100

    s

--- index.php
+++ index.php
@@ -16,19 +16,32 @@
     <title>Practice</title>
 </head>
 <body>
-<p>
-    <?php
-    echo "Hello World";
-    ?>
-</p>
-<h1>
-    <?php
-    $myname = "Harry Asin";
-    $myage = 26;
-    echo "My Name is ". $myname . " and i am " . $myage;
-    echo $myage;
-    ?>
-</h1>
+<h1>SPECS, MUGS & SAUSAG ROLLS</h1>
+<form method = "post" action ="index.php">
+    Name: <input type = "text" name = "name">
+    Age: <input type = "number" name="age">
+    <input type = "submit" name = "submit" value=" Submit">
+</form>
+
+<?php
+$myage = $_POST ['age'];
+$name = $_POST ['name'];
+
+if ($myage >= 21){
+    print "You can buy Specs, Mugs & Sausages";
+}
+elseif($myage >= 18){
+    print "You can buy Specs & Mugs only";
+}
+elseif($myage >= 16){
+    print "You can buy only Specs";
+}
+elseif($myage > 16){
+    print "Come back when you are 16";
+}
+
+?>
+
 
 </body>
 </html>
