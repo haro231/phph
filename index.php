908fb618fb0bbe495bae13e856f711f711c41d85
Haro231 <1419172@rgu.ac.uk>
2016-05-03 17:10:19 +0100

    s

--- index.php
+++ index.php
@@ -9,5 +9,20 @@
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<p>
+    <?php
+    echo "Hello World";
+    ?>
+</p>
 
+
+</body>
+</html>
 
